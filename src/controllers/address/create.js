709b406cbeb75a437e6c1b5317507b28eb0c58e0
cchampou <clement@champouillon.com>
2020-03-24 00:11:18 +0100

    With address

--- src/controllers/address/create.js
+++ src/controllers/address/create.js
@@ -1,6 +1,23 @@
 
-const create = () => {
+const { logError } = require('@cda/logger');
 
+const { checkFields } = require('../../utils/request');
+const { SERV_ERR, OK, MISS_PARAM } = require('../../messages');
+
+const Address = require('../../models/address');
+
+const create = async ({ body }, res) => {
+  try {
+    const fields = ['gender', 'name', 'firstname', 'line1', 'zipcode', 'city', 'country'];
+    if (!checkFields(body, fields)) {
+      return res.status(400).send(MISS_PARAM);
+    }
+    await Address.insertOne(body);
+    return res.status(200).send(OK);
+  } catch (e) {
+    logError(e);
+    return res.status(500).send(SERV_ERR);
+  }
 };
 
 
--- src/models/address.js
+++ src/models/address.js
@@ -7,11 +7,9 @@ const { getClient } = require('../database');
 const insertOne = async (data) => {
   logDebug('Inserting one address');
   const client = getClient();
-  const rows = await client.insert(R.pick(['gender', 'firstname', 'name', 'line1', 'line2', 'zipcode', 'city', 'country'], data)).into('');
-  logDebug(rows);
+  await client.insert(R.pick(['gender', 'firstname', 'name', 'line1', 'line2', 'zipcode', 'city', 'country'], data)).into('address');
   logDebug('Success');
 };
-
 
 module.exports = {
   insertOne,
