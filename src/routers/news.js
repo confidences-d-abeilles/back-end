b5fa88b246f324e6c842fc8cc308a25fe7bf5713
cchampou <clement@champouillon.com>
2020-03-24 00:38:26 +0100

    With new creation

---
+++ src/routers/news.js
@@ -0,0 +1,26 @@
+
+const { Router } = require('express');
+
+const router = Router();
+
+const Hive = require('../controllers/beehive');
+
+/**
+ * @api {post} /beehive Creates a beehive
+ * @apiName CreateBeehive
+ * @apiGroup Beehive
+ * @apiVersion 1.0.0
+ *
+ * @apiParam {String} name Name of the beehive
+ * @apiParam {String} identifier Technical identifier
+ * @apiParam {String} occupation Current occupation in percent
+ * @apiParam {String} memo A quick note space
+ *
+ * @apiErrorExample 400
+ *  HTTP 400 Missing parameters
+ *
+ * @apiSuccess {String} message OK
+ */
+router.post('/', Hive.create);
+
+module.exports = router;
