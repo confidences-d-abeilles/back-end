fb453489f7ee29713448d0d1b8d4bcf975e069fb
cchampou <clement@champouillon.com>
2020-03-29 22:55:40 +0200

    With jsdoc

--- docs/api_project.js
+++ docs/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-03-29T20:44:34.880Z",
+    "time": "2020-03-29T20:55:11.329Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
