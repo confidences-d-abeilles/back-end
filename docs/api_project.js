6d5698d9b7888c2f9329c4aa13405cd1506ee873
cchampou <clement@champouillon.com>
2020-03-29 14:36:13 +0200

    Change doc name

--- docs/api_project.js
+++ docs/api_project.js
@@ -2,14 +2,14 @@ define({
   "name": "The brand new User API for CDA",
   "version": "1.0.0",
   "description": "",
-  "title": "User API",
-  "url": "https://auth.cda.cchampou.me",
+  "title": "Bee API",
+  "url": "https://xxx.cchampou.me",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-03-29T11:11:51.702Z",
+    "time": "2020-03-29T12:35:59.028Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
--- src/models/beehive.js
+++ src/models/beehive.js
@@ -3,7 +3,7 @@ const BaseModel = require('./base');
 
 class Beehive extends BaseModel {
   constructor(data = {}) {
-    super('beehive', ['id', 'name', 'occupation', 'identifier', 'memo'], data);
+    super('beehive', ['id', 'name', 'occupation', 'identifier', 'memo', 'lat', 'long', 'temp_in', 'temp_out', 'hygrometry'], data);
   }
 }
 
