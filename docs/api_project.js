1501ed5129f6e31a87d1045513efd6d8783fef5f
cchampou <clement@champouillon.com>
2020-03-29 23:24:22 +0200

    Fix

--- docs/api_project.js
+++ docs/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-03-29T21:18:39.762Z",
+    "time": "2020-03-29T21:21:43.416Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
